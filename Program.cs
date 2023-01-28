11a378340382f88933a7e4e672c4d04ae330f13b
Sergey <skarpov@gmail.com>
2023-01-28 19:42:23 +0300

    Edited: code optimised and tested

--- Program.cs
+++ Program.cs
@@ -2,27 +2,29 @@
 меньше, либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать
 на старте выполнения алгоритма*/
 
-string text = "A string is a sequential collection of characters that's used to represent text."+ 
-                "A String object is a sequential collection of System.Char objects that represent a string"+
-                "a System.Char object corresponds to a UTF-16 code unit. The value of the String object is "+
-                "the content of the sequential collection of System.Char objects, and that value is immutable "+
-                "(that is, it is read-only). For more information about the immutability of strings, see the "+
-                "Immutability and the StringBuilder class section. The maximum size of a String object in memory "+
+// set input data
+string text = "A string is a sequential collection of characters that's used to represent text." +
+                " A String object is a sequential collection of System.Char objects that represent a string;" +
+                " a System.Char object corresponds to a UTF-16 code unit. The value of the String object is " +
+                "the content of the sequential collection of System.Char objects, and that value is immutable " +
+                "(that is, it is read-only). For more information about the immutability of strings, see the " +
+                "Immutability and the StringBuilder class section. The maximum size of a String object in memory " +
                 "is 2-GB, or about 1 billion characters.";
 
+Console.Clear();
+// output string array taken from original string
+PrintData("ИСХОДНЫЙ МАССИВ:");
+Console.ForegroundColor = ConsoleColor.Yellow;
+PrintArray(FillArrayFromTxt(text)); Console.WriteLine();
+Console.ResetColor();
 
-Console.WriteLine (text);
-string [] mainArray = FillArrayFromTxt (text);
- PrintArray(mainArray);
-
+// output resulting array
+PrintData("ИТОГОВЫЙ МАССИВ:");
+Console.ForegroundColor = ConsoleColor.Green;
+PrintArray(CreateArrayWithSelectedItems(FillArrayFromTxt(text)));
+Console.ResetColor();
 
 // METHODS
-int ReadData(string msg)        // input data method
-{
-    Console.Write(msg);
-    return int.Parse(Console.ReadLine() ?? "0");
-}
-
 void PrintData(string msg)      // print data method
 {
     Console.WriteLine(msg);
@@ -42,13 +44,37 @@ void PrintArray(string[] col)      // print array method
     Console.WriteLine();
 }
 
-string [] FillArrayFromTxt (string line) // method for fill string array from text 
+string[] FillArrayFromTxt(string line) // method for fill string array from text 
 {
-    line = line.Replace(",", "");
-    string[] names = line.Split(" ");
-    return names;
+    line = line.Replace(",", "").Replace(".", "");
+    string[] strArray = line.Split(" ");
+    return strArray;
 }
 
+string[] CreateArrayWithSelectedItems(string[] arr) // method for creating new array with selected elements from input array
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            count++;        // counting elements with lenght < 4 chars
+        }
+    }
+
+    string[] resultArr = new string[count];     // creating array with lenght = numbers of elements with lenght < 4 chars in input array
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        if (arr[i].Length < 4)
+        {
+            resultArr[index] = arr[i];      // coping elements to new array
+            index++;
+        }
+    }
+    return resultArr;
+}
 
 
 
